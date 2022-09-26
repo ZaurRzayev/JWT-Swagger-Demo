6fc12520a1ef42808a431f21c22252f8b4e7dc2f
Zaur Rzayev <61623422+ZaurRzayev@users.noreply.github.com>
2022-09-26 15:58:06 +0400

    token upload

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -6,10 +6,15 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -21,13 +26,36 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     private static final String LICENSE = "Apache License Version 2.0";
     private static final String LICENSE_URL = "http://www.apache.org/licenses/LICENSE-2.0";
 
+//    @Bean
+//    public Docket productApi() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+//                .build()
+//                .apiInfo(metaData());
+//    }
+
     @Bean
-    public Docket productApi() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("az.afb.payment"))
-                .build()
-                .apiInfo(metaData());
+                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                //.paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My REST API",
+                "Some custom description of API.",
+                "1.0",
+                "Terms of service",
+                new Contact(".", ".", "."),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
     }
 
     private ApiInfo metaData() {
@@ -39,6 +67,23 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .licenseUrl(LICENSE_URL)
                 .build();
     }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,16 +1,33 @@
 package com.example.demo.controller;
 
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/auth/")
+import java.util.Arrays;
+import java.util.List;
+
+//@RestController
+//public class HelloController {
+//
+//
+//    @GetMapping("/hi")
+//    public String greetings(){
+//        return "Start with success";
+//    }
+//
+//}
+
+
+@RestController(value = "/clients")
+@Api( tags = "Clients")
 public class HelloController {
 
-    @GetMapping("hi")
-    public String greetings(){
-        return "Start with success";
+    @ApiOperation(value = "This method is used to get the clients.")
+    @GetMapping
+    public List<String> getClients() {
+        return Arrays.asList("First Client", "Second Client");
     }
 }
